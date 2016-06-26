e20139ec2f691f1dd7b59f0d12c8902e7f62a986
codetracer <joker0910@sina.com>
2016-06-26 13:04:16 +1000

    1 add tanimoto coefficient

--- grouping/clusters.py
+++ grouping/clusters.py
@@ -46,6 +46,15 @@ def pearson(v1, v2):
 
 	return 1.0 - num/den
 
+def tanimoto(v1, v2):
+	c1, c2, shr = 0,0,0
+
+	for i in range(len(v1)):
+		if v1[i] != 0: c1+=1 # in v1
+		if v2[i] != 0: c2+=1 # in v2
+		if v1[i] != 0 and v2[i] != 0:
+			shr += 1 # in both
+	return 1.0 - (float(shr)/(c1+c2-shr))
 
 class bicluster:
 	def __init__(self, vec, left = None, right = None, \
@@ -228,5 +237,8 @@ transdata = rotatematrix(data)
 newclust = hcluster(transdata)
 drawdendrogram(newclust, labels = words, jpeg = 'wordsclust.jpeg')
 
-
+# clusetering the zebo.txt
+wants, people, data = readfile('zebo.txt')
+clust = hcluster(data, distance = tanimoto)
+drawdendrogram(clust, wants, jpeg = 'zebo.jpeg')
 

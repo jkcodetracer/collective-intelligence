9fd3d91f5295825b25368c4735514f27d820def6
codetracer <joker0910@sina.com>
2016-11-14 11:00:31 +1000

    hill climbing algorithm & simulated annealing algorithm

--- optimization/optimization.py
+++ optimization/optimization.py
@@ -19,27 +19,27 @@ def getminutes(t):
 	return x[3]*60 + x[4]
 
 def printschedule(r):
-	bd = len(r)/2
-	for d in range(len(r)/2):
-		print '%d %d' % (d, d+bd)
-		name = people[d][0]
-		origin = people[d][1]
+	d = 0
+	while (d < len(r)):
+		name = people[d/2][0]
+		origin = people[d/2][1]
 		out = flight[(origin, destination)][r[d]]
-		ret = flight[(destination, origin)][r[d+bd]]
+		ret = flight[(destination, origin)][r[d+1]]
 		print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % \
 			(name, origin, out[0], out[1], out[2], \
 			ret[0], ret[1], ret[2])
+		d += 2
 
 def schedulecost(sol):
 	totalprice = 0
 	latestarrival = 0
 	earliestdep = 24 * 60
-	bd = len(sol)/2
+	d = 0
 
-	for d in range(len(sol)/2):
-		origin = people[d][1]
+	while (d < len(sol)):
+		origin = people[d/2][1]
 		outbound = flight[(origin, destination)][int(sol[d])]
-		returnf = flight[(destination, origin)][int(sol[d+bd])]
+		returnf = flight[(destination, origin)][int(sol[d+1])]
 
 		totalprice += outbound[2]
 		totalprice += returnf[2]
@@ -51,14 +51,18 @@ def schedulecost(sol):
 		if earliestdep > getminutes(returnf[0]):
 			earliestdep = getminutes(returnf[0])
 
+		d+=2
+
 	# wait for other people
 	totalwait = 0
-	for d in range(len(sol)/2):
-		origin = people[d][1]
+	d = 0
+	while (d < len(sol)):
+		origin = people[d/2][1]
 		outbound = flight[(origin, destination)][int(sol[d])]
-		returnf = flight[(destination, origin)][int(sol[d+bd])]
+		returnf = flight[(destination, origin)][int(sol[d+1])]
 		totalwait += latestarrival - getminutes(outbound[1])
 		totalwait += getminutes(returnf[0]) - earliestdep
+		d += 2
 
 	if latestarrival > earliestdep:
 		totalprice += 50
@@ -82,6 +86,68 @@ def randomoptimize(domain, costf):
 
 	return r
 
+def hillclimb(domain, costf):
+	#create random solution
+	sol = [random.randint(domain[i][0], domain[i][1]) \
+		for i in range(len(domain))]
+
+	# main loop
+	while 1:
+		# create a list of neighbouring solution
+		# from two direction
+		neighbors = []
+		for j in range(len(domain)):
+			if sol[j] > domain[j][0]:
+				neighbors.append(sol[0:j]+\
+					[sol[j]-1]+sol[j+1:])
+			if sol[j] < domain[j][1]:
+				neighbors.append(sol[0:j]+\
+					[sol[j]+1]+sol[j+1:])
+
+		# find the best among neighbors
+		current = costf(sol)
+		best = current
+		for j in range(len(neighbors)):
+			cost = costf(neighbors[j])
+			if cost < best:
+				best = cost
+				sol = neighbors[j]
+		if best == current:
+			break;
+	return sol;
+
+def annealingoptimize(domain, costf, T=10000.0, cool=0.95, step=1):
+	# Initialize the values randomly
+	vec = [float(random.randint(domain[i][0], domain[i][1])) \
+		for i in range(len(domain))]
+
+	while T>0.1:
+		i = random.randint(0, len(domain)-1)
+
+		# choose a direction to change it
+		dif = random.randint(-step, step)
+		# create a new values
+		vecb = vec[:]
+		vecb[i] += dir
+		if vecb[i] < domain[i][0]:
+			vecb[i] = domain[i][0]
+		elif vecb[i] > domain[i][1]:
+			vecb[i] = domain[i][1]
+
+		# compare the cost
+		ea = costf(vec)
+		eb = costf(vecb)
+		# simulated annealing
+		p = pow(math.e, (-eb-ea)/T)
+
+		# is it better, or make the probability
+		if (eb < ea or random.random() < p):
+			vec = vecb
+
+		T = T*cool
+	return vec
+
+
 
 flight = {}
 for line in file('schedule.txt'):
@@ -99,6 +165,11 @@ s = randomoptimize(domain, schedulecost)
 print '--- test random ---'
 print schedulecost(s)
 print printschedule(s)
+
+print '--- test hillclimb ---'
+s = hillclimb(domain, schedulecost)
+print schedulecost(s)
+printschedule(s)
 
 
 

e79b508f6d4083ea464c8021431a6c78fe358a6a
codetracer <joker0910@sina.com>
2016-11-23 23:58:49 +0800

    add variance
    1 if the predictive dimension is int or float, we’d better use variance
    to calculate the score.

--- decisiontree/treepredict.py
+++ decisiontree/treepredict.py
@@ -46,6 +46,13 @@ def entropy(rows):
 		ent = ent - p*log2(p)
 	return ent
 
+def variance(rows):
+	if len(rows) == 0:
+		return 0
+	data = [float(row[len(row)-1]) for row in rows]
+	mean = sum(data)/len(data)
+	variance = sum([(d-mean)**2 for d in data])/len(data)
+	return variance
 
 # put rows in two sets based on the choosen column
 def divideset(rows, column, value):

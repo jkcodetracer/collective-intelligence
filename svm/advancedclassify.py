e16d5961063282c258deecdaa811095c2bdfadbf
codetracer <joker0910@sina.com>
2016-11-25 22:53:20 +0800

    transfer all the data into numerical forms and scale all the data

--- svm/advancedclassify.py
+++ svm/advancedclassify.py
@@ -8,11 +8,73 @@ class matchrow:
 			self.data = row[0:len(row)-1]
 		self.match = int(row[len(row)-1])
 
+def yesno(v):
+	if v == 'yes':
+		return 1
+	elif v == 'no':
+		return -1
+	else:
+		return 0
+def matchcount(interest1, interest2):
+	l1 = interest1.split(':')
+	l2 = interest2.split(':')
+	x = 0
+	for v in l1:
+		if v in l2:
+			x += 1
+	return x
+
+def milesdistance(a1, a2):
+	return 0
+
+
 def loadmatch(f, allnum=False):
 	rows = []
 	for line in file(f):
 		rows.append(matchrow(line.split(','), allnum))
 	return rows
+
+def loadnumerical():
+	oldrows = loadmatch('matchmaker.csv')
+	newrows = []
+
+	for row in oldrows:
+		d = row.data
+		data = [float(d[0]), yesno(d[1]), yesno(d[2]),
+			float(d[5]), yesno(d[6]), yesno(d[7]),
+			matchcount(d[3], d[8]),
+			milesdistance(d[4],d[9]),
+			row.match]
+		newrows.append(matchrow(data))
+	return newrows
+
+# put all the value in [0,1]
+def scaledata(rows):
+	low = [999999999.0]*len(rows[0].data)
+	high = [-999999999.0]*len(rows[0].data)
+
+	for row in rows:
+		d = row.data
+		for i in range(len(d)):
+			if d[i] < low[i]:
+				low[i] = d[i]
+			if d[i] > high[i]:
+				high[i] = d[i]
+
+	# create a function that scales data
+	def scaleinput(du):
+		output = []
+		for i in range(len(low)):
+			if high[i] == low[i]:
+				output += [0]
+			else:
+				output += [(du[i]-low[i])/(high[i]-low[i])]
+		return output
+
+	newrows = [matchrow(scaleinput(row.data) + [row.match])
+			for row in rows]
+
+	return newrows, scaleinput
 
 def lineartrain(rows):
 	averages = {}
@@ -55,4 +117,16 @@ print dpclassify([30,30], avgs)
 print dpclassify([30,25], avgs)
 print dpclassify([25,40], avgs)
 print dpclassify([48,20], avgs)
+
+numericalset = loadnumerical()
+print numericalset[0].data
+
+print '--- test scale ---'
+scaledataset, scalef = scaledata(numericalset)
+avgs = lineartrain(scaledataset)
+print numericalset[0].data
+print numericalset[0].match
+print dpclassify(scalef(numericalset[0].data), avgs)
+print numericalset[11].match
+print dpclassify(scalef(numericalset[11].data), avgs)
 
